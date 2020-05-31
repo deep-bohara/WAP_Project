e0fec78de6a4df69ec24ff5b28724a85db213606
deep_bohara <dpak23bohara@gmail.com>
2020-05-30 20:46:40 -0500

    Text Readonly

--- Lesson6/test.js
+++ Lesson6/test.js
@@ -1,14 +1,34 @@
-function
-    b() {
-    function
-        a() {
-        console.log(x);
-    }
-    var
-        x = 10;
-    a();
-    console.log(x);
-}
 var
-    x = 20;
-b();
+    x = 10;
+function
+    main() {
+    console.log(
+        log("x1 is " + x);
+    x =
+        20;
+    console.log(
+        log("x2 is " + x);
+    if
+        (x > 0) {
+        x =
+            30; // x=
+        console.log(
+            log("x3 is " + x);
+    }
+    console.log(
+        log("x4 is " + x);
+    var
+        x = 40; // x=
+    var
+        f = function (x) {
+            console.log(
+                log("x5 is " + x);
+        }
+    f(
+        50);
+    console.log(
+        log("x6 is " + x);
+}
+main();
+console.log(
+    log("x7 is " + x);
--- Lesson7/Account.js
+++ Lesson7/Account.js
@@ -4,6 +4,7 @@ var accountModule = (function () {
     var privateMethodCreateAccount = function () {
 
         acName = document.getElementById("ac").value;
+        console.log(acName);
         bal = document.getElementById("dep").value;
         accountInfoList.push({
             acName: acName,
@@ -27,11 +28,16 @@ function pageLoad() {
 
 }
 function display() {
+    document.getElementById("acinfo").value="";
+    for(const acInfo of accountInfoList) {
+        document.getElementById("acinfo").value+= "\nAccount Name:" + acInfo.acName + " " + "Balance:" +acInfo.bal;
+// console.log(acInfo);
+}
 
-    nam = document.getElementById("ac").value;
-    bal = document.getElementById("dep").value;
-    // info=document.getElementById("acinfo");
-    document.getElementById("acinfo").value += "\nAccount Name:" + nam + " " + "Balance:" + bal;
+    // nam = document.getElementById("ac").value;
+    // bal = document.getElementById("dep").value;
+    // // info=document.getElementById("acinfo");
+    // document.getElementById("acinfo").value += "\nAccount Name:" + nam + " " + "Balance:" + bal;
 }
 
 window.onload = pageLoad;
