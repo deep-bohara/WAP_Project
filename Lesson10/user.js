e57f1e8724ef12fad1f5462d39fca0f8f5f6306a
deep_bohara <dpak23bohara@gmail.com>
2020-06-05 16:02:33 -0500

    Comment

--- Lesson10/user.js
+++ Lesson10/user.js
@@ -20,6 +20,26 @@ $(function () {
 
     });
 
+    var displayComments = function (commentBtn, comments) {
+        var commentContainer = $(commentBtn).parents('.post-container').find('.comment-box');
+        for (var key in comments) {
+            var singleComment = comments[key];
+            var body = $('<p>').text(singleComment.body);
+            var commentedBy = $('<p>').text(`Commnted By: ${singleComment.name}`);
+            commentContainer.append(body)
+            commentContainer.append(commentedBy)
+        }
+    }
+
+    $(document).on('click', '.comment-btn', function (e) {
+        var postId = $(this).parents('.post-container').attr('data-key');
+
+        fetch(`${url}/comments?postId=${postId}`)
+            .then(function (resp) {
+                return resp.json();
+            }).then(comments => displayComments(this, comments));
+    })
+
     $("#pbtn").on("click", function (e) {
         var userid = $("#user-form").find('[name="userid"]').val();
         // alert(userid);
@@ -29,14 +49,15 @@ $(function () {
             })
             .then(function (posts) {
                 $('#user-post').text('');
-                for(key in posts){
-                    var  single_post = posts[key];
-                    var post_container = $('<div class="post-container">');
+                for (key in posts) {
+                    var single_post = posts[key];
+                    single_post.id = parseInt(single_post.id);
+                    var post_container = $(`<div class="post-container" data-key="${single_post.id}">`);
                     var title = $('<h3>').text(single_post.title);
                     var body = $('<p>').text(single_post.body);
-                    var commentBtn = $('<button>').addClass('comment-btn').val(single_post.id).text('Viw Comment..');
+                    var commentBtn = $('<button>').addClass('comment-btn').val(single_post.id).text('View Comment..');
                     var commentBox = $('<div>').addClass('comment-box');
-                    
+
                     post_container.append(title);
                     post_container.append(body);
                     post_container.append(commentBtn);
@@ -44,15 +65,45 @@ $(function () {
 
                     $('#user-post').append(post_container);
                 }
-                $('.comment-btn').on('click',function(e){
-                    alert('kkk');
-                })
-               
+                // $('document').on('click', '.comment-btn', function (e) {
+                //     // alert('kkk');
+
+                //     var postid = $("this").parents('.single_post').attr('data-key');
+                //     alert(postid);
+                //     var single_post = posts[key].postId;
+
+                //     fetch(`${url}/comments?postId=${single_post}`)
+                //         .then(function (response) {
+                //             return response.json;
+                //         })
+                //         .then(function (comments) {
+                //             $('#comment-box').text('');
+                //             for (key in comments) {
+                //                 var single_comment = comments[key];
+                //                 var name = $('<h2>').text(single_comment.name);
+                //                 var email = $('<p>').text(single_comment.email);
+                //                 var body = $('<h2>').text(single_comment.body);
+
+                //                 post_container.append(name);
+                //                 post_container.append(email);
+                //                 post_container.append(body);
+
+                //                 $('.comment-box').append(post_container);
+
+
+                //             }
+
+
+                //         })
+                //     // alert(single_post);
+
+                // })
+
             })
 
 
     })
-   
+
     // $(document).on('click','.comment-btn',function(e){
     //     alert('kkk');
     // })
@@ -62,7 +113,7 @@ $(function () {
 
 
 
-    // 
+     
 
 
 
@@ -72,13 +123,3 @@ $(function () {
 
 
 
-// let url='http://jsonplaceholder.typicode.com';
-// let data={username:'deepak'};
-// fetch(url,{
-// method:'POST',
-// body:JSON.stringify(data),
-// headers:{
-// 'content-type':'application/json'
-// }
-
-// })
