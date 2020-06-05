1d1c414486a4d34b2a5ed8f3315af68c12010332
deep_bohara <dpak23bohara@gmail.com>
2020-06-04 20:13:55 -0500

    AJAX

---
+++ Lesson10/output.js
@@ -0,0 +1,29 @@
+$(function(){
+    "use strict";
+
+$(function() {
+	$('#hw').change(function() {
+		var file = 'homeworks/'+$('#hw').val();
+		$.ajax({
+			'url': file,
+			'type': 'GET',
+			'success': ajaxSuccess,
+			'error': ajaxFailure
+		});
+	});
+});
+
+function ajaxSuccess(data) {
+	$('#output').val(data);s
+}
+
+function ajaxFailure(xhr, status, exception) {
+  console.log(xhr, status, exception);
+}
+
+$.get('https://www.google.com/images/branding/product/ico/googleg_lodp.ico');
+
+
+
+});
+
